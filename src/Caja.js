899e250eb46a224dbc19dfb421ad13d2e1cf2c50
Lucasdn2003 <lucas.dnavarro@gmail.com>
2022-06-13 21:28:55 -0300

    First commit

---
+++ src/Caja.js
@@ -0,0 +1,11 @@
+function Caja(props) {
+    return (
+      <div className="Caja">
+       <p>
+           Buenas Noches {props.nombre}!
+        </p>
+      </div>
+    );
+  }
+  
+  export default Caja;
